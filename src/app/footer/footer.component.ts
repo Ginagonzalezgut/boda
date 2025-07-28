49938245f60845abb918405943a7edf47ec60ead
Gina González <gini_go@hotmail.com>
2025-07-28 22:33:56 +0200

    Track click link

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 
+declare let gtag: Function;
+
 @Component({
   selector: 'app-footer',
   imports: [MenuComponent],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  
+  trackClickGina() {
+    gtag('event', 'click_link', {
+      event_category: 'Click de link',
+      event_label: 'Gina',
+    });
+  }
 }
